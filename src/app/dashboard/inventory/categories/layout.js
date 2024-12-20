335e1c00a0f0edcd03a7b103c0c75595640795ba
Onwuasoanya James <onwuasoanyajames@gmail.com>
2024-12-20 20:32:20 +0100

    nexshelf12/20 main/master(1)

--- src/app/dashboard/inventory/categories/layout.js
+++ src/app/dashboard/inventory/categories/layout.js
@@ -1,15 +1,20 @@
 import { Button } from "@/components/ui/button";
 import { Plus, Search } from "lucide-react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 
 export default function CategoriesLayout({ children }) {
   
   return (   
-    <div className='flex flex-col pl-2 pr-1 pt-2 overflow-hidden h-full'>
+    <Tabs defaultValue="products" className='flex flex-col pl-2 pr-1 pt-2 overflow-hidden h-full'>
           <header className="bg-white rounded-md p-2 font-Inter ">
-            <p className="font-bold">Categories</p>
+            <p className="font-bold px-1">Categories</p>
+            <TabsList className={`inline-grid w-fit mt-1 overflow-x-hidden gap-3 bg-blue-400-200 rounded-[3px] border-black py-2 h-fit grid-cols-3`}>
+                    <TabsTrigger className='py-1 px-2 relative border-transparent rounded data-[state=active]:shadow-none data-[state=active]:border-t data-[state=active]:text-white bg-slate-200 data-[state=active]:bg-slate-800' value="products">Products</TabsTrigger>
+                    <TabsTrigger className='py-1 px-2 relative border-transparent rounded data-[state=active]:shadow-none data-[state=active]:border-t data-[state=active]:text-white bg-slate-200 data-[state=active]:bg-slate-800' value="brand">Brand</TabsTrigger>
+            </TabsList>
           </header>
           {children}
-    </div>    
+    </Tabs>    
   );
 }
--- src/app/dashboard/inventory/categories/page.js
+++ src/app/dashboard/inventory/categories/page.js
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { MoveLeftIcon, Plus, Search } from "lucide-react"
 import { useState } from "react";
 import { Switch } from "@/components/ui/switch";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 
 const Categories = () => {
@@ -11,10 +12,16 @@ const Categories = () => {
     const [activeInterface, setActiveInterface] = useState('landing')
     return (
       <div className="bg-white flex flex-col h-full overflow-y-scroll flex-grow rounded-md p-1 mt-2">
+        <TabsContent value="products" className='mt-1 p-1'>
             {
                 activeInterface === 'landing'? <CategoryLanding changeInterface={()=>setActiveInterface('add category')}/>: 
                 activeInterface === 'add category'?<AddCategory changeInterface={()=>setActiveInterface('landing')}/> : ""              
-            }    
+            }   
+        </TabsContent>
+        <TabsContent value="brand" className='mt-1 p-1'>
+          <div className="mx-auto h-5 w-5 my-2 bg-red-500"></div>
+        </TabsContent>
+           
       </div>
     )
   }
@@ -43,39 +50,48 @@ const AddCategory = ({changeInterface}) => {
       <div className="text-xs flex ">
         <Button onClick={changeInterface} className='p-2' variant='ghost'>
           <MoveLeftIcon className="w-4 h-4 mr-1"/>
-          <span className="text-gray-500 text-xs font-light">All categories</span>
+          <span className="text-gray-500 text-xs font-light">Product categories</span>
         </Button>
       </div>
-      <p className="font-semibold px-2 mt-4 text-base">Create new category</p>
-      <div className="p-2">
-        <div className='mt-2 font-Inter relative'>
-          <label htmlFor="" className='relative ml-1 font-medium items-center flex justify-between text-sm mb-[1px]' >
-            <span> Select product type</span> 
+      <p className="font-semibold px-2 mt-3 text-sm">Create product category</p>
+      <div className="p-[0.4rem]">
+        <div className='mt-3 font-Inter relative'>
+          <label htmlFor="" className='relative ml-1 font-semibold text-gray-500 items-center flex justify-between text-xs mb-[1px]' >
+            <span> Industry/Professional category</span> 
           </label>
           <ComboboxDemo addType />
-          <p className="text-gray-400 text-[0.69rem] p-1 font-extralight">Product type will be the default parent category for the category you're about to create. It is generally classified by nature of the products to guide creators.</p>
+          <p className="text-gray-400 text-[0.69rem] leading-normal italic -mt-1 p-1 font-extralight">Note: This is a predefined category for all products. It provides a more structured framework that guides product creators to add more product specific categories.</p>
         </div>
-        <div className='mt-3 relative'>
-          <label htmlFor="" className='relative ml-[2px] font-medium items-center flex justify-between text-sm mb-[1px]' >
+        <div className='mt-2 relative'>
+          <label htmlFor="" className='relative ml-[2px] font-semibold text-gray-500 items-center flex justify-between text-xs mb-[1px]' >
             <span> Category name</span> 
           </label>
-          <input id='heading-text' className='border rounded w-[14rem] text-sm outline-none p-2' type='text' />
+          <input id='heading-text' className='border rounded-md w-[14rem] text-sm outline-none p-[0.4rem]' type='text' />
           {/* <p className="absolute pl-1 text-[10px] text-red-400 italic">error secttion</p> */}
-          <p className="text-gray-400 text-[0.69rem] font-light ml-2">All categories</p>
+          {/* <p className="text-gray-400 text-[0.69rem] leading-tight italic p-1 font-extralight">All categories</p> */}
         </div>
         <div className='mt-2 font-Inter relative'>
-          <label htmlFor="" className='relative ml-1 font-medium items-center flex justify-between text-sm mb-[1px]' >
-            <span> choose parent category</span> 
+          <label htmlFor="" className='relative ml-1 font-semibold text-gray-500 items-center flex justify-between text-xs mb-[1px]' >
+            <span> Choose parent category</span> 
           </label>
-          <div className='flex mx-3 my-2 justify-between items-center'>
-            <p className="inline-block text-xs">enable shared parents</p>
-            <div className="relative top-1 ">
-                <Switch checked={shared} onCheckedChange={()=>setShared(!shared)} />
-            </div>
-          </div>
           <ComboboxDemo addType />
-          <p className="text-gray-400 text-[0.69rem] p-1 font-extralight">Product type will be the default parent category for the category you're about to create. It is generally classified by nature of the products to guide creators.</p>
+          <p className="text-gray-400 text-[0.69rem] leading-tight italic p-1 -mt-1 font-extralight">Select from categories already created.</p>
         </div>
+        <div className='mt-2 font-Inter relative'>
+          <label htmlFor="" className='relative ml-1 font-semibold text-gray-500 items-center flex justify-between text-xs mb-[1px]' >
+            <span> Slug</span> 
+          </label>
+          <input id='heading-text' className='border rounded-md w-[14rem] text-sm outline-none p-[0.4rem]' type='text' />
+          <p className="text-gray-400 text-[0.69rem] leading-tight italic p-1 -mt-1 font-extralight">The 'slug' is the url friendly version of the name. it is usually all lowercase and contains only letters, numbers and hyphens.</p>
+        </div>
+        <div className='mt-2 font-Inter relative'>
+          <label htmlFor="" className='relative ml-1 font-semibold text-gray-500 items-center flex justify-between text-xs mb-[1px]' >
+            <span> Description</span> 
+          </label>
+          <textarea id='heading-text' className='border rounded-md w-[14rem] text-sm outline-none p-[0.4rem]' rows={3}></textarea>
+          {/* <p className="text-gray-400 text-[0.69rem] leading-tight italic p-1 -mt-1 font-extralight">The 'slug' is the url friendly version of the name. it is usually all lowercase and contains only letters, numbers and hyphens.</p> */}
+        </div>
+        <Button onClick={changeInterface} className='mt-2 text-xs font-semibold' size='sm'>Create</Button>
       </div>
     </div>
   )
